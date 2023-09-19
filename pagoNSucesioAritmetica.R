692537927680bb00a2067f7f5417d10c13909f6f
Darina <144275019+daricrnn@users.noreply.github.com>
2023-09-19 10:31:08 -0700

    Add files via upload

---
+++ pagoNSucesioAritmetica.R
@@ -0,0 +1,12 @@
+#pagoNSucesioAritmetica
+#Darina Calderon Rivera
+: Sn=25000
+n=72
+d=50
+Xn=((2*Sn)/n)+((n-1)*d)/2
+Xn
+pagoNSucesionAritmética<- function(Sn,n,d)
+  }
+Xn=((2*Sn)/n)+((n-1)*d)/2
+}
+Xn
